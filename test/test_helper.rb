39c57ae00046657abd787a01b6085e4b312ccdcf
0v0d <legolesia@gmail.com>
2025-03-27 16:24:51 +0900

    Complete advanced testing setup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,15 +1,15 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require "minitest/reporters"
+Minitest::Reporters.use!
 
 module ActiveSupport
   class TestCase
-    # Run tests in parallel with specified workers
+    # 指定のワーカー数でテストを並列実行する
     parallelize(workers: :number_of_processors)
-
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+    # test/fixtures/*.yml にあるすべての fixture をセットアップする
     fixtures :all
-
-    # Add more helper methods to be used by all tests here...
+    # （すべてのテストで使うその他のヘルパーメソッドは省略）
   end
 end
